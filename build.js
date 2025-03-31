036aff890d9d731998c0e71ec75637e6a992bedd
elena-aguilar <elenaguilardesign@gmail.com>
2025-03-31 18:04:39 +0000

    wip cambios

--- build.js
+++ build.js
@@ -1,6 +1,70 @@
+import StyleDictionary from 'style-dictionary';
+import { expandTypesMap, register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+import { format } from 'path';
+
+register(StyleDictionary, {
+     withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
 async function run() {
-     //console.log('hola mundo');
-     //esto es un comentario
+     const themes = await loader.load('/tokens');
+
+     const globalTheme = themes.getThemeByName('global');
+     const lightTheme= themes.getThemeByName('light');
+
+     const globalConfig = {
+          expand: {
+               typesMap: true,
+          },
+          platforms: {
+               web: {
+                    files: [
+                         {
+                              destination: 'app/build/global/variables.css',
+                              format: 'css/variables',
+                         }
+                    ],
+
+                    transforms: [
+                         'name/kebab',
+                         'ts/resolveMath',
+                         'ts/typography/fontWeight',
+                         'ts/size/lineheight',
+                         'size/pxToRem'
+                    ]
+                    
+               }
+
+          }
+     };
+
+     // app/build/light/variables.css
+
+     const lightConfig = {
+          platforms: {
+               web: {
+                    files: [
+                         {
+                              destination: 'app/build/light/variables.css',
+                              format: 'css/variables',
+                         }
+                         
+                    ],
+
+                    transforms: [
+                         'name/kebab',
+                         'color/rgb',
+                    ]
+               }
+          }
+     };
+
+     globalTheme.addConfig(globalConfig).build();
+     lightTheme.addConfig(lightConfig).build();
+
 
 }
 
